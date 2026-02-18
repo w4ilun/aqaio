f2a26277d72766f266d8a9b545871a9c3dab4873
w4ilun <w4ilun@users.noreply.github.com>
2026-02-17 20:29:13 -0500

    add time and thresholds

--- components/aqaio/__init__.py
+++ components/aqaio/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.components import sensor, time as time_
+from esphome.const import CONF_ID, CONF_TIME_ID
 
 CODEOWNERS = []
 
@@ -18,6 +18,10 @@ CONF_EPD_RST = "epd_rst"
 CONF_EPD_BUSY = "epd_busy"
 CONF_DISPLAY_ROTATION = "display_rotation"
 CONF_USE_FAHRENHEIT = "use_fahrenheit"
+CONF_AQ_CO2_MAX = "aq_co2_max"
+CONF_AQ_VOC_MAX = "aq_voc_max"
+CONF_AQ_NOX_MAX = "aq_nox_max"
+CONF_AQ_PM_MAX = "aq_pm_max"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -33,6 +37,11 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_EPD_BUSY, default=2): cv.int_,
             cv.Optional(CONF_DISPLAY_ROTATION, default=0): cv.int_range(0, 3),
             cv.Optional(CONF_USE_FAHRENHEIT, default=False): cv.boolean,
+            cv.Optional(CONF_TIME_ID): cv.use_id(time_.RealTimeClock),
+            cv.Optional(CONF_AQ_CO2_MAX, default=1000): cv.int_range(min=0),
+            cv.Optional(CONF_AQ_VOC_MAX, default=150): cv.int_range(min=0),
+            cv.Optional(CONF_AQ_NOX_MAX, default=1): cv.int_range(min=0),
+            cv.Optional(CONF_AQ_PM_MAX, default=12): cv.int_range(min=0),
         }
     )
     .extend(cv.polling_component_schema("60s"))
@@ -56,6 +65,16 @@ async def to_code(config):
     )
     cg.add(var.set_display_rotation(config[CONF_DISPLAY_ROTATION]))
     cg.add(var.set_use_fahrenheit(config[CONF_USE_FAHRENHEIT]))
+    cg.add(var.set_aq_thresholds(
+        config[CONF_AQ_CO2_MAX],
+        config[CONF_AQ_VOC_MAX],
+        config[CONF_AQ_NOX_MAX],
+        config[CONF_AQ_PM_MAX],
+    ))
+
+    if time_id := config.get(CONF_TIME_ID):
+        time_var = await cg.get_variable(time_id)
+        cg.add(var.set_time(time_var))
 
     cg.add_library("Wire", None)
     cg.add_library("SPI", None)
@@ -64,5 +83,3 @@ async def to_code(config):
     cg.add_library("adafruit/Adafruit BusIO", "1.16.2")
     cg.add_library("adafruit/Adafruit GFX Library", "1.11.11")
     cg.add_library("zinggjm/GxEPD2", None)
-
-
